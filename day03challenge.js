0dee36c8c2ddc0d7ede188ba9a57946296c2848a
Mohammed Sanaved <mosanaved@gmail.com>
2023-05-11 01:22:32 +0530

    initial

--- day03challenge.js
+++ day03challenge.js
@@ -24,3 +24,59 @@
 // } else {
 //     console.log(`${number} is not divisible by 2`)
 // }
+
+
+
+// let number = Number(prompt('enter a number'));
+
+// if (number % 2 == 0 && number < 15) {
+//     console.log('print A');
+// } else if (number % 2 == 0 && number > 15) {
+//     console.log('print B');
+// } else if (number % 2 != 0 && number < 10) {
+//     console.log('print C');
+// } else if (number % 2 != 0 && number > 10) {
+//     console.log('print D');
+// }
+// let myName = 'Sanaved';
+// myName = 'Mr'+" "+myName
+// console.log(myName);
+
+// let sum = 0;
+// let multiple = 1;
+
+// for (let i = 10; i <= 20; i++) {
+//     sum = sum + i;
+//     multiple = multiple * i;
+// }
+// console.log(sum);
+// console.log(multiple);
+
+// let user = prompt('Check the Current Season');
+// console.log(user);
+// if (user === 'September') {
+//     console.log(`the ${user} month is Autumn`);
+// } else if (user === 'Octuber') {
+//     console.log(`the ${user} month is Autumn`);
+// }  else if (user === 'November') {
+//     console.log(`the ${user} month is Autumn`);
+// }  else if (user === 'January') {
+//     console.log(`the ${user} month is Winter`);
+// }  else if (user === 'February') {
+//     console.log(`the ${user} month is Winter`);
+// }  else if (user === 'December') {
+//     console.log(`the ${user} month is Winter`);
+// }  else if (user === 'June') {
+//     console.log(`the ${user} month is Summer`);
+// }  else if (user === 'July') {
+//     console.log(`the ${user} month is Summer`);
+// }  else if (user === 'August') {
+//     console.log(`the ${user} month is Summer`);
+// }  else if (user === 'March') {
+//     console.log(`the ${user} month is Spring`);
+// }  else if (user === 'April') {
+//     console.log(`the ${user} month is Spring`);
+// }  else if (user === 'May') {
+//     console.log(`the ${user} month is Spring`);
+// }
+
---
+++ day04challenge.js
@@ -0,0 +1,18 @@
+let webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+console.log(webTechs);
+let webTechs2 = ['PHP','Python','Java','SQL','Graph'];
+let arr1 = webTechs.push('GraphQl');
+console.log(arr1);
+console.log(webTechs);
+let arr2 = webTechs2.pop();
+console.log(arr2);
+let str = webTechs2.join();
+console.log(str);
+let strtoarr = 'we are going to play soccer';
+console.log(strtoarr.split(' '));
+console.log(webTechs.unshift('Hall'));
+console.log(webTechs.sort());
+console.log(webTechs.slice(0,5))
+console.log(webTechs.splice(2, 1));
+
+let tech = webTechs.concat(webTechs2); 
