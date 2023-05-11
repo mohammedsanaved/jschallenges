274921cc173a263089a1d16c6baf9279593a9014
Mohammed Sanaved <mosanaved@gmail.com>
2023-05-11 17:29:27 +0530

    day06 challenge

--- day05challenge.js
+++ day05challenge.js
@@ -104,3 +104,77 @@ const person = {
   })
 
 console.log(users);
+
+const usersInfo = [
+    {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt: '08/01/2020 9:00 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt: '08/01/2020 9:30 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt: '08/01/2020 9:45 AM',
+      isLoggedIn: true,
+    },
+    {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt: '08/01/2020 9:50 AM',
+      isLoggedIn: false,
+    },
+    {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt: '08/01/2020 10:00 AM',
+      isLoggedIn: false,
+    },
+  ]
+  console.log(usersInfo)
+
+  function addUser(id, username, email, password) 
+  {
+
+    const date = new Date();
+const options = {
+  month: '2-digit',
+  day: '2-digit',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: true
+};
+const formattedDate = date.toLocaleString('en-US', options);
+// console.log(formattedDate); // Output: "08/01/2020 9:50 AM"
+
+    const user ={
+        _id: id,
+                username: username,
+                email: email,
+                password: password,
+                createdAt: formattedDate,
+                isLoggedIn: false,
+    }
+    usersInfo.push(user);
+  }
+
+  addUser('ab12ex', 'Alex', 'alex@alex.com', '123123');
+  addUser('ab23ex', 'willson', 'will@son.com', '223123');
+  console.log(usersInfo);
---
+++ day06challenge.js
@@ -0,0 +1,60 @@
+function sumTwoNumbers(numOne, numTwo) {
+    let sum = numOne + numTwo
+    return sum
+  }
+  console.log(sumTwoNumbers(12,12));
+
+
+function sumall () {
+    let sum = 0
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i]
+    }
+    return sum;
+}
+console.log(sumall(1,2,3,4,5,6,7,8,9,10));
+
+function evensandOdds (num) {
+    let sum = 0;
+    let arr = [];
+    for (let i = 1; i <= num  ; i++) {
+        if(i % 2 === 0){
+            arr.push(i)
+        } 
+    }
+    console.log(`the even number are ${arr.length}`);
+}
+console.log(evensandOdds(25));
+
+
+function ajay() {
+    let jmaa = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        jmaa += arguments[i]
+    }
+    console.log(jmaa)
+}
+console.log(ajay(1,2,3,4));
+
+function rangile() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.trunc(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`
+}
+console.log(rangile());
+
+
+function getRandomHexColor() {
+    // Generate three random numbers between 0 and 255
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+  
+    // Convert the RGB values to a hex string
+    const hex = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  
+    return hex;
+  }
+  console.log(getRandomHexColor());
